213e0bb223e4fe3e7c800b49482821d7d3879d51
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-17 12:34:38 -0400

    Ajouter un nom à la table Categories

--- resto/database/migrations/2024_05_17_162848_create_categories_table.php
+++ resto/database/migrations/2024_05_17_162848_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nom')->nullable(false)->unique();
         });
     }
 
